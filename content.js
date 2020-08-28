02fe656a6eaf120da26e9422def0e80c635ea7cc
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2020-08-28 11:17:38 +0100

    :tada: early birds

---
+++ content.js
@@ -0,0 +1,29 @@
+console.log('bmlet ready!');
+
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage (message, sender, sendResponse) {
+  console.log('get in herererere', message);
+  // if(message.txt == 'hey there!') {
+  //   console.log('yes ofcos')
+  //   let paragraphs = document.getElementsByTagName('p');
+  //   for(el of paragraphs) {
+  //     el.style['background-color'] = '#FF00FF';
+  //   }
+  // }
+}
+//
+// (function(){
+//     console.log('bookmarklet init');
+//     let paragraphs = document.getElementsByTagName('p');
+//     for(i = 0; i < paragraphs.length; i++) {
+//       paragraphs[i].innerHTML = 'heyy';
+//     }
+// })();
+//
+// (function() {
+//   let script = document.createElement('script');
+//   script.src = 'bookmarklet.js';
+//   document.body.appendChild(script);
+// }())
